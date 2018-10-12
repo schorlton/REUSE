5014896451d609fb0c32d99d86bbae28fc875e71
innovate.invent@gmail.com <nolan@i2labs.ca>
2018-10-12 15:28:47 -0700

    Add thread pool.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
+#include <vector>
+#include <thread>
+
+
+#include "SharedQueue.h"
+
+using Record = char*;
+using Queue = SharedQueue<Record>;
+
+//Filter thread interface declaration
+void filter(Queue, Queue);
 
 int main() {
     std::cerr << "Hello, World!" << std::endl;
+    uint queue_limit = 10; //Default soft limit for queue before thread pool increase
     //Parse and validate parameters
 
     //Init thread pool
+    Queue pending_records, output_records;
+    std::vector<std::thread> thread_pool;
+    thread_pool.emplace(thread_pool.end(), filter, pending_records, output_records);
 
     //Read in records to queue
 
     //Check queue size and increase thread pool to desaturate
+    if (pending_records.size() > queue_limit) thread_pool.emplace(thread_pool.end(), filter, pending_records, output_records);
 
     //Join thread pool
 
