0b8703b2517459d80a0e6b9d1fec0bf92b0b26ce
innovate.invent@gmail.com <nolan@i2labs.ca>
2018-10-12 14:29:49 -0700

    Add ThreadQueue

---
+++ ThreadQueue.cpp
@@ -0,0 +1,63 @@
+//
+// Created by Nolan Woods on 10/12/2018.
+//
+
+#include "ThreadQueue.h"
+
+
+template <typename T>
+SharedQueue<T>::SharedQueue(){}
+
+template <typename T>
+SharedQueue<T>::~SharedQueue(){}
+
+template <typename T>
+T& SharedQueue<T>::front()
+{
+    std::unique_lock<std::mutex> mlock(mutex_);
+    while (queue_.empty())
+    {
+        cond_.wait(mlock);
+    }
+    return queue_.front();
+}
+
+template <typename T>
+void SharedQueue<T>::pop_front()
+{
+    std::unique_lock<std::mutex> mlock(mutex_);
+    while (queue_.empty())
+    {
+        cond_.wait(mlock);
+    }
+    queue_.pop_front();
+}
+
+template <typename T>
+void SharedQueue<T>::push_back(const T& item)
+{
+    std::unique_lock<std::mutex> mlock(mutex_);
+    queue_.push_back(item);
+    mlock.unlock();     // unlock before notificiation to minimize mutex con
+    cond_.notify_one(); // notify one waiting thread
+
+}
+
+template <typename T>
+void SharedQueue<T>::push_back(T&& item)
+{
+    std::unique_lock<std::mutex> mlock(mutex_);
+    queue_.push_back(std::move(item));
+    mlock.unlock();     // unlock before notificiation to minimize mutex con
+    cond_.notify_one(); // notify one waiting thread
+
+}
+
+template <typename T>
+int SharedQueue<T>::size()
+{
+    std::unique_lock<std::mutex> mlock(mutex_);
+    int size = queue_.size();
+    mlock.unlock();
+    return size;
+}
---
+++ ThreadQueue.h
@@ -0,0 +1,35 @@
+//
+// Created by Nolan Woods on 10/12/2018.
+// Taken from https://stackoverflow.com/a/36763257
+//
+
+#ifndef REUSE_THREADQUEUE_H
+#define REUSE_THREADQUEUE_H
+
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+
+template <typename T>
+class SharedQueue
+{
+    public:
+        SharedQueue();
+        ~SharedQueue();
+
+        T& front();
+        void pop_front();
+
+        void push_back(const T& item);
+        void push_back(T&& item);
+
+        int size();
+        bool empty();
+
+    private:
+        std::deque<T> queue_;
+        std::mutex mutex_;
+        std::condition_variable cond_;
+};
+
+#endif //REUSE_THREADQUEUE_H
