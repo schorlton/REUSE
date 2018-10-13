ba2e09d3623bad55cc41ced30458d139abd0b16a
innovate.invent@gmail.com <nolan@i2labs.ca>
2018-10-13 10:08:05 -0700

    Revert "Move threading code to different branch"
    This reverts commit 28be4c4

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <vector>
+#include <thread>
 #include <cstring>
 
 #include "cmdline.h"
+#include "SharedQueue.h"
 
+using Record = char*;
+using Queue = SharedQueue<Record>;
+
+//Thread interface declaration
+void filter(Queue, Queue);
+void output(Queue);
 
 int reuse_build(int argc, char **argv){
 
@@ -10,17 +19,27 @@ int reuse_build(int argc, char **argv){
 }
 
 int reuse_filter(int argc, char **argv){
-
+    uint queue_limit = 10; //Default soft limit for queue before thread pool increase
     //Parse and validate parameters
 
     //Init thread pool
-
+    Queue pending_records, output_records;
+    std::vector<std::thread> thread_pool;
+    thread_pool.emplace(thread_pool.end(), filter, pending_records, output_records);
+    thread_pool.emplace(thread_pool.end(), output, output_records);
 
     //Read in records to queue
+    while (false) { //TODO while (records remain to be read)
+        //Push record into queue
 
-
-    //Join thread pool
+        //Check queue size and increase thread pool to desaturate
+        if (pending_records.size() > queue_limit)
+            thread_pool.emplace(thread_pool.end(), filter, pending_records, output_records);
+            while (pending_records.size(false) > queue_limit);
+    }
     
+    //Join thread pool
+    for (auto& thread : thread_pool) thread.join();
 
     //Output statistics
 
