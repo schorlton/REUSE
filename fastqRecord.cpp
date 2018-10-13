6319075ee1dea69a7e8493d0a1d628d63b74cb02
jasmeet9323 <jasmeet.singh.mec11@iitbhu.ac.in>
2018-10-13 13:22:59 -0700

    Add fastqRecord structure to read fastq sequences: derived from fasta structure

---
+++ fastqRecord.cpp
@@ -0,0 +1,17 @@
+/*
+ * fastqRecord.cpp
+ *
+ *  Created on: Oct 13, 2018
+ *      Author: jassi
+ */
+#include<fastaRecord.h>
+#include<fastqRecord.h>
+#include<seqan/seq_io.h>
+#include<seqan/sequence.h>
+
+void FastqRecord::FastqRecord(seqan::CharString id, seqan::Dna5QString seq, seqan::CharString qual){
+	this->id = id;
+	this->seq = seq;
+	this->qual = qual;
+}
+
