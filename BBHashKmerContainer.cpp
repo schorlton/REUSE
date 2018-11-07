38a0feefe0e40dd286afe2188868b7c1d90fee66
innovate.invent@gmail.com <nolan@i2labs.ca>
2018-11-06 16:19:03 -0800

    Refactor BBHashKmerContainer to follow convention

---
+++ BBHashKmerContainer.cpp
@@ -0,0 +1,81 @@
+//
+// Created by Nolan Woods on 11/6/2018.
+//
+
+#include <fstream>
+
+#include <memory>
+
+#include "BBHashKmerContainer.h"
+
+template<typename Iter, typename charType>
+void BBHashKmerContainer<Iter, charType>::load(const char *fname) {
+    std::ifstream stream{fname};
+    bphf->load(stream);
+}
+
+template<typename Iter, typename charType>
+void BBHashKmerContainer<Iter, charType>::save(const char *fname) {
+    std::ofstream stream{fname};
+    bphf->save(stream);
+}
+
+template<typename Iter, typename charType>
+BBHashKmerContainer<Iter, charType>::BBHashKmerContainer() : BBHashKmerContainer(1, 1.0, 1) {
+}
+
+template<typename Iter, typename charType>
+BBHashKmerContainer<Iter, charType>::BBHashKmerContainer(int numThreads, double gammaFactor, int numElements, int k) {
+    this->numThreads = numThreads;
+    this->gammaFactor = gammaFactor;
+    this->numElements = numElements;
+    kmers_size = k;
+    bphf = std::make_unique<boomphf::mphf<u_int64_t, hasher_t>>();
+}
+
+template<typename Iter, typename charType>
+bool BBHashKmerContainer<Iter, charType>::contains(charType *s) {
+    //TODO change to seqan type
+    KmerIterator<charType> iter(s, kmers_size);
+    // kmer_encode(iter, iter+kmers_size);
+    //query mphf like this
+    uint64_t idx = bphf->lookup(kmer_encode(iter, iter + kmers_size));
+    // printf(" example query  %lli ----->  %llu \n",data[0],idx);
+
+    return idx != static_cast<uint64_t>(-1);
+}
+
+template<typename Iter, typename charType>
+void BBHashKmerContainer<Iter, charType>::add(charType *s, int length) {
+    //TODO change to seqan type
+    //TODO Implement
+}
+
+
+template<typename Iter, typename charType>
+void BBHashKmerContainer<Iter, charType>::addRange(Iter &start, Iter &end) {
+
+    //double t_begin,t_end; struct timeval timet;
+
+    //printf("Construct a BooPHF with  %lli elements  \n", numElements);
+
+    //gettimeofday(&timet, NULL); t_begin = timet.tv_sec +(timet.tv_usec/1000000.0);
+
+    // mphf takes as input a c++ range. A simple array of keys can be wrapped with boomphf::range
+    // but could be from a user defined iterator (enabling keys to be read from a file or from some complex non-contiguous structure)
+    auto data_iterator = boomphf::range(start, end);
+
+    //build the mphf
+    bphf = std::make_unique<boomphf::mphf<u_int64_t, hasher_t>>(numElements, data_iterator, numThreads, gammaFactor);
+
+    //gettimeofday(&timet, NULL); t_end = timet.tv_sec +(timet.tv_usec/1000000.0);
+    //double elapsed = t_end - t_begin;
+
+
+    //printf("BooPHF constructed perfect hash for %llu keys in %.2fs\n", numElements, elapsed);
+    //printf("boophf  bits/elem : %f\n",(float) (bphf->totalBitSize())/numElements);
+
+
+    // free(data);
+
+}
--- BBHashKmerContainer.h
+++ BBHashKmerContainer.h
@@ -10,118 +10,40 @@
 #include <sys/types.h>
 #include <random>
 #include <algorithm>
-#include <iostream>
+
 
 //tells bbhash to use included hash function working on u_int64_t input keys :
-typedef boomphf::SingleHashFunctor<u_int64_t>  hasher_t;
-typedef boomphf::mphf<  u_int64_t, hasher_t  > boophf_t;
+using hasher_t = boomphf::SingleHashFunctor<u_int64_t>;
+using boophf_t = boomphf::mphf<u_int64_t, hasher_t>;
 
-template <typename Iter, typename charType>
-class BBHashKmerContainer: public AbstractKmerContainer<Iter, charType> {
-	public:
-		// default constructor
-	    BBHashKmerContainer();   
-        ~BBHashKmerContainer();
-	    // variable constructor
-	    BBHashKmerContainer(int numThreads, double gammaFactor, int numElements, int k);
+template<typename Iter, typename charType>
+class BBHashKmerContainer : public AbstractKmerContainer<Iter, charType> {
+public:
+    // default constructor
+    BBHashKmerContainer();
 
-	    bool contains(charType* s); //TODO change to seqan type
-	    void add(charType* s, int length); //TODO change to seqan type
+    ~BBHashKmerContainer() = default;
 
-	    void addRange(Iter& start, Iter& end);
+    // variable constructor
+    BBHashKmerContainer(int numThreads, double gammaFactor, int numElements, int k);
 
-		void load(const char *file);
+    bool contains(charType *s); //TODO change to seqan type
+    void add(charType *s, int length); //TODO change to seqan type
 
-		void save(const char *file);
+    void addRange(Iter &start, Iter &end);
+
+    void load(const char *fname);
+
+    void save(const char *fname);
 
 private:
-		int kmers_size;
-		std::unique_ptr<boophf_t> bphf;
-		int numThreads;
-		double gammaFactor;
-		int numElements;
+    int kmers_size;
+    std::unique_ptr<boophf_t> bphf;
+    int numThreads;
+    double gammaFactor;
+    int numElements;
 };
 
-
-template <typename Iter, typename charType>
-void BBHashKmerContainer<Iter,charType>::load(const char *fname){
-
-
-    std::ifstream ifn(fname);
-
-    bphf->load(ifn);
-}
-template <typename Iter, typename charType>
-void BBHashKmerContainer<Iter,charType>::save(const char *fname){
-
-    std::ofstream out(fname);
-
-    bphf->save(out);
-}
-template <typename Iter, typename charType>
-BBHashKmerContainer<Iter, charType>::~BBHashKmerContainer(){
-}
-
-template <typename Iter, typename charType>
-BBHashKmerContainer<Iter, charType>::BBHashKmerContainer() {
-	BBHashKmerContainer(1, 1.0, 1);
-}
-
-template <typename Iter, typename charType>
-BBHashKmerContainer<Iter, charType>::BBHashKmerContainer(int numThreads, double gammaFactor, int numElements, int k) {
-	this->numThreads = numThreads;
-	this->gammaFactor = gammaFactor;
-	this->numElements = numElements;
-	kmers_size = k;
-
-	bphf = std::make_unique<boomphf::mphf<u_int64_t,hasher_t>>();
-}
-
-template <typename Iter, typename charType>
-bool BBHashKmerContainer<Iter, charType>::contains(charType* s) {
-	//TODO change to seqan type
-    KmerIterator<charType> iter(s, kmers_size);
-	// kmer_encode(iter, iter+kmers_size);
-	//query mphf like this
-	uint64_t idx = bphf->lookup(kmer_encode(iter, iter+kmers_size));
-	// printf(" example query  %lli ----->  %llu \n",data[0],idx);
-
-	return idx != static_cast<uint64_t>(-1);
-}
-
-template <typename Iter, typename charType>
-void BBHashKmerContainer<Iter, charType>::add(charType* s, int length) {
-	//TODO change to seqan type
-} 
-
-
-template <typename Iter, typename charType>
-void BBHashKmerContainer<Iter, charType>::addRange(Iter& start, Iter& end) {
-
-	//double t_begin,t_end; struct timeval timet;
-	
-	//printf("Construct a BooPHF with  %lli elements  \n", numElements);
-
-	//gettimeofday(&timet, NULL); t_begin = timet.tv_sec +(timet.tv_usec/1000000.0);
-
-	// mphf takes as input a c++ range. A simple array of keys can be wrapped with boomphf::range
-	// but could be from a user defined iterator (enabling keys to be read from a file or from some complex non-contiguous structure)
-	auto data_iterator = boomphf::range(start, end);
-
-	//build the mphf
-	bphf = std::make_unique<boomphf::mphf<u_int64_t,hasher_t>>(numElements, data_iterator, numThreads, gammaFactor);
-	
-	//gettimeofday(&timet, NULL); t_end = timet.tv_sec +(timet.tv_usec/1000000.0);
-	//double elapsed = t_end - t_begin;
-	
-	
-	//printf("BooPHF constructed perfect hash for %llu keys in %.2fs\n", numElements, elapsed);
-	//printf("boophf  bits/elem : %f\n",(float) (bphf->totalBitSize())/numElements);
-	
-
-	// free(data);
-
-}
-
+#include "BBHashKmerContainer.cpp"
 
 #endif
