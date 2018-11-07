41831403676b076cd5894b508e51cc5457268a80
innovate.invent@gmail.com <nolan@i2labs.ca>
2018-11-06 17:58:04 -0800

    Add timer based status output and code comments.

--- build.cpp
+++ build.cpp
@@ -2,6 +2,9 @@
 // Created by Nolan Woods on 11/6/2018.
 //
 
+#include <iostream>
+#include <ctime>
+
 #include <seqan/sequence.h>
 #include <seqan/seq_io.h>
 #include <seqan/basic.h>
@@ -11,29 +14,31 @@
 
 using KmerContainer = BBHashKmerContainer<KmerIterator<seqan::Dna5>, seqan::Dna5>;
 
+const double update_interval = 1; //Seconds
+
 void print_build_status(unsigned long total_sequences, unsigned long total_kmers) {
     if (total_sequences)
         //Delete previous output
         std::cerr << "\033[F\033[K";
-    std::cerr << " Total processed: " << std::setw(10) << total_sequences << " Total kmers: " << std::setw(10)
-              << total_kmers << std::endl;
+    std::cerr << " Total processed: " << std::setw(10) << total_sequences << " Total kmers: " << std::setw(10) << total_kmers << std::endl;
 }
 
-int build(ParametersBuild &params) {
+int build(ParametersBuild& params) {
 
     std::cerr << "Building reference......" << std::endl;
 
+    //Open input sequence file
     seqan::SeqFileIn seqfile;
-
     if (params.seq_filename == "-") {
         open(seqfile, std::cin);
     } else {
         open(seqfile, params.seq_filename.c_str());
     }
 
+
+    //Read in records
     seqan::StringSet<seqan::CharString> ids;
     seqan::StringSet<seqan::Dna5String> seqs;
-
     try {
         readRecords(ids, seqs, seqfile);
     } catch (seqan::Exception const &e) {
@@ -44,15 +49,24 @@ int build(ParametersBuild &params) {
     int refTableLength = length(seqs[0]) / 2 - 21; //TODO document this line
     KmerContainer table(params.threads, 2, refTableLength, params.kmer_length); //TODO replace hardcoded params
 
-    for (unsigned i = 0; i < length(ids); ++i) {
+    //Generate and hash kmers
+    std::time_t last_time{std::time(nullptr)}, now;
+    for (unsigned long i = 0; i < length(ids); ++i) {
 
         KmerIterator<seqan::Dna5> _begin = get_begin(toCString(seqs[i]), params.kmer_length);
         KmerIterator<seqan::Dna5> _end = get_end(toCString(seqs[i]), length(seqs[i]), params.kmer_length);
 
         std::cerr << "Length : " << length(seqs[i]) << std::endl;
         table.addRange(_begin, _end);
+
+        now = std::time(nullptr);
+        if (std::difftime(last_time, now) > update_interval) {
+            last_time = now;
+            print_build_status(i, 0); //TODO replace 0 with something meaningful
+        }
     }
 
+    //Save hash table to file
     table.save(params.output_filename.c_str());
 
 
